ed18b32ce7e3030174d72ae2ca08943fdec3a722
Michiel Huygen <mhgamework@gmail.com>
2015-08-22 15:47:41 +0200

    Prey can now spot you!

---
+++ Assets/Scripts/DetectPlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class DetectPlayerScript : MonoBehaviour {
+
+    public FirstPersonController Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject != Player.gameObject) return;
+
+        Debug.Log("Game over!");
+    }
+}
