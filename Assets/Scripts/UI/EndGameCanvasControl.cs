ea1a4ba9a146a1b05bbafa8d31301da45a95dc3b
Ewout Van Gossum <ewoutvangossum@gmail.com>
2015-08-22 14:51:08 +0200

    Added beginning and ending screens.

---
+++ Assets/Scripts/UI/EndGameCanvasControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class EndGameCanvasControl : MonoBehaviour
+{
+    [SerializeField]
+    private UIFader uiFader;
+
+    private bool isVisible;
+    private bool gameEnded;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    uiFader = this.GetComponent<UIFader>();
+        uiFader.Fade(0,0,EasingFunctions.TYPE.Out);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.A))
+	    {
+	        showEndGameCanvas();
+	    }
+	    if (isVisible)
+	    {
+	        if (Input.GetKey(KeyCode.F) && !gameEnded)
+	        {
+	            endGame();
+	        }
+	    }
+	}
+
+    public void endGame()
+    {
+        gameEnded = true;
+        Debug.Log("The game has ended.");
+    }
+
+
+    public void showEndGameCanvas()
+    {
+        uiFader.Fade(1,0.2f,EasingFunctions.TYPE.In);
+        isVisible = true;
+    }
+}
---
+++ Assets/Scripts/UI/StartGameCanvasControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class StartGameCanvasControl : MonoBehaviour
+{
+    [SerializeField]
+    private UIFader uiFader;
+
+    private bool isVisible = true;
+
+    private bool gameStarted;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    uiFader = this.GetComponent<UIFader>();
+        uiFader.Fade(1,0,EasingFunctions.TYPE.In);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (isVisible)
+	    {
+	        if (Input.GetKey(KeyCode.S) && !gameStarted)
+	        {
+                hideStartGameCanvas();
+	            startGame();
+	        }
+	    }
+	}
+
+    public void startGame()
+    {
+        gameStarted = true;
+        Debug.Log("The game has started.");
+    }
+
+
+    public void hideStartGameCanvas()
+    {
+        uiFader.Fade(0,0.2f,EasingFunctions.TYPE.Out);
+        isVisible = false;
+    }
+}
