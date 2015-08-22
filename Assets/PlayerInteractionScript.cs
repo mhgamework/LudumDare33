818bade20de55b6271d3c668bb24e41702c8fe36
Jasper Hilven <jasperhilven@gmail.com>
2015-08-22 23:30:02 +0200

    Lightswitch => working

---
+++ Assets/PlayerInteractionScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteractionScript : MonoBehaviour
+{
+    public float RaycastDistance = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.F))
+        { 
+            var ray = Camera.main.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
+
+            RaycastHit hitInfo;
+            if (!Physics.Raycast(ray, out hitInfo, RaycastDistance)) return;
+
+            var lss = hitInfo.collider.transform.GetComponentInParent<LightSwitchScript>();
+            if (lss != null)
+            {
+                lss.Interact();
+            }
+
+        }
+	}
+}
---
+++ Assets/Scripts/LightSwitchScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitchScript : MonoBehaviour {
+
+    private bool lightOn = false;
+    [SerializeField]
+    private GameObject lightCone;
+
+	// Use this for initialization
+	void Start () {
+        lightOn = true;
+        lightCone.SetActive(lightOn);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    internal void Interact()
+    {
+        lightOn = !lightOn;
+        lightCone.SetActive(lightOn);
+    }
+}
