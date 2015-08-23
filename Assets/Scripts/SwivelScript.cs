7c78f68edf2d80cf36f977c52e1b0a40de72f719
Ewout Van Gossum <ewoutvangossum@gmail.com>
2015-08-23 11:16:20 +0200

    Added SwivelScript. For swiveling lights yo.

---
+++ Assets/Scripts/SwivelScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwivelScript : MonoBehaviour
+{
+    [SerializeField] private GameObject thingToSwivel = null;
+    [SerializeField] private float swivelStartPosition = -30f;
+    [SerializeField] private float swivelEndPosition = 30f;
+    [SerializeField] private float swivelSpeed = 1f;
+
+    private float currentAngle;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    thingToSwivel.transform.localRotation = Quaternion.AngleAxis(swivelStartPosition,Vector3.forward);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var range = swivelEndPosition - swivelStartPosition;
+        var rotationAngle = sawtooth(Time.timeSinceLevelLoad/range*swivelSpeed);
+	    rotationAngle = rotationAngle*range - swivelStartPosition;
+
+        thingToSwivel.transform.localRotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
+
+	}
+
+    float sawtooth(float x)
+    {
+        return Mathf.Abs((x%1)*2 - 1);
+    }
+}
