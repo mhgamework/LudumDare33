0e81cfa8ddeeeede4e8715951cfb8f487a736def
Michiel Huygen <mhgamework@gmail.com>
2015-08-23 10:53:24 +0200

    Cleaned up prefabs, added box, damaginglight and flickerlight

---
+++ Assets/EditorSphere.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditorSphere : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        
+        Gizmos.DrawSphere(transform.position, transform.localScale.magnitude);
+    }
+
+}
