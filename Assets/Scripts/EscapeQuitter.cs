c3ee25861c54f5e29e754627504b942e88ffd5c0
Simon Chuptys <simon.chuptys@gmail.com>
2015-08-24 14:16:05 +0200

    Added game quit functionality.

---
+++ Assets/Scripts/EscapeQuitter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeQuitter : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            QuitGame();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
