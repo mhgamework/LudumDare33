e1e862ac2bbd38244b13eaf54e4f2eac80c1f7dd
Simon Chuptys <simon.chuptys@gmail.com>
2015-08-24 13:39:16 +0200

    Added lock+hide cursor script.

---
+++ Assets/Scripts/CursorHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorHandler : MonoBehaviour
+{
+    [SerializeField]
+    private bool CursorVisible = false;
+    [SerializeField]
+    private bool LockCursor = true;
+
+    void Start()
+    {
+        Cursor.visible = CursorVisible;
+        Cursor.lockState = LockCursor ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
