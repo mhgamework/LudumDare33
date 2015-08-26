0ed681f0b55682961af7818ab8ade9cfaa19b73b
MHGW-Laptop <mhgamework@gmail.com>
2015-08-26 16:57:21 +0200

    Added snapping for the tiles and tweaked the asset prefabs, no rotation yet.

---
+++ Assets/Scripts/EditorSnappingScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [ExecuteInEditMode]
+    public class EditorSnappingScript : MonoBehaviour
+    {
+        public float SnapSize = 1;
+        public Vector3 Offset = new Vector3();
+
+        public void Update()
+        {
+            if (Application.isPlaying) return;
+            if (SnapSize == 0) SnapSize = 1;
+            var pos = transform.position;
+            pos += Offset;
+            pos.x = Mathf.Round(pos.x / SnapSize) * SnapSize;
+            pos.y = Mathf.Round(pos.y / SnapSize) * SnapSize;
+            pos.z = Mathf.Round(pos.z / SnapSize) * SnapSize;
+
+            pos -= Offset;
+            transform.position = pos;
+        }
+    }
+}
